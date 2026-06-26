27f4e757d8ee5923675bf7c307a9abd0d0ddb9c1
Youngsun Kang <yskang1984@gmail.com>
2026-06-26 20:41:02 +0900

    Update site title

--- docs/assets/js/aba21aa0.d65a7c48.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktemp_site=self.webpackChunktemp_site||[]).push([["3747"],{7093(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/assets/js/acecf23e.10a22744.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktemp_site=self.webpackChunktemp_site||[]).push([["3220"],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
