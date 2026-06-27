9103c7acceeec7f47aedb350c59707cfde794a28
Youngsun Kang <yskang1984@gmail.com>
2026-06-27 22:28:44 +0900

    Deploy Docusaurus build output

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\GitHub\\Eric841001.github.io\\src\\css\\custom.css"),
+];
