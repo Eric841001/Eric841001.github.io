997becdab3a8cd888e859c2c008b6a6a91c97618
Youngsun Kang <yskang1984@gmail.com>
2026-06-28 15:58:09 +0900

    Deploy website - based on 778f871d30e89f5efc4ef050986125272c9614c9

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\GitHub\\Eric841001.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\GitHub\\Eric841001.github.io\\src\\css\\custom.css"),
-];
